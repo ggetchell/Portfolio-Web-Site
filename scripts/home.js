adc35a795ae628300b5199c9edf7ea1f27c2bc4f
Gregory Getchell <ggetchell@gmail.com>
2016-10-19 06:30:34 -0400

    Updated Web Files

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Configure/customize these variables.
-    var showChar = 100;  // How many characters are shown by default
+    var showChar = 50;  // How many characters are shown by default
     var ellipsestext = "...";
     var moretext = "Show more >";
     var lesstext = "Show less";
